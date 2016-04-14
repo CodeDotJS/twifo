78c132a9dc8607c5bcd15a485ed9eb2afbb8f3fe
CodeDotJS <rushforlinux@gmail.com>
2016-04-14 22:01:53 +0530

    not scrapped

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 'use strict';
 
 var cheerio = require('cheerio');
-var got = require('got');
+
 var Promise = require('pinkie-promise');
+
+var got = require('got');
+
+module.exports = function (username) {
+	if (typeof username !== 'string') {
+		return Promise.reject(new Error('username required'));
+	}
+
+	var url = 'https://www.twitter.com/' + username;
+
+	return got(url).then(function (res) {
+		var $ = cheerio.load(res.body);
+
+		return {
+			name: $('a.ProfileHeaderCard-nameLink').text() || null,
+
+			handle: $('.ProfileHeaderCard-screennameLink').attr('href') || null,
+
+			curryTweet: $('.js-tweet-text[1]').text() || null,
+
+			bio: $('.ProfileHeaderCard-bio').text() || null,
+
+			tweets: $('.ProfileNav-value[0]').text() || null,
+
+			followers: $('.ProfileNav-value[2]').text() || null,
+
+			following: $('.ProfileNav-value[1]').text() || null,
+
+			likes: $('.ProfileNav-value[3]').text() || null
+		};
+	}).catch(function (err) {
+		if (err.statusCode === 404) {
+			err.message = 'Not a Twitter User.';
+		}
+
+		throw err;
+	});
+};
