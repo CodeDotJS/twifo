ff547525071d9ea69f71e2beb28aef16fb01467b
CodeDotJS <rushforlinux@gmail.com>
2016-04-15 08:34:49 +0530

    passed

---
+++ test.js
@@ -0,0 +1,14 @@
+import test from 'ava';
+import fn from './';
+
+test(async t => {
+	const user = await fn('pakalupapito');
+
+	t.is(user.name, 'pakalu papito');
+	t.is(user.handle, 'pakalupapito');
+	t.is(user.bio, 'started from gas station now we here.');
+	t.is(user.tweets, '2,240');
+	t.is(user.followers, '1.51M');
+	t.is(user.following, '174');
+	t.is(user.likes, '2,349');
+});
