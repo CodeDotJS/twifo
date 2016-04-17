e03ec4c6475df0d3a66839ef750f75a1cc95a34d
CodeDotJS <rushforlinux@gmail.com>
2016-04-18 02:16:24 +0530

    fixed

--- test.js
+++ test.js
@@ -7,8 +7,8 @@ test(async t => {
 	t.is(user.name, 'pakalu papito');
 	t.is(user.handle, 'pakalupapito');
 	t.is(user.bio, 'started from gas station now we here.');
-	t.is(user.tweets, '2,243');
-	t.is(user.followers, '1.51M');
-	t.is(user.following, '178');
+	t.is(user.tweets, '2,246');
+	t.is(user.followers, '1.52M');
+	t.is(user.following, '177');
 	t.is(user.likes, '2,348');
 });
