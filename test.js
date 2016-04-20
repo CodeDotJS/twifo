dd5f904d2559244066bbb1a12f6ea00de8bc4589
CodeDotJS <rushforlinux@gmail.com>
2016-04-21 01:30:10 +0530

    passed

--- test.js
+++ test.js
@@ -7,8 +7,8 @@ test(async t => {
 	t.is(user.name, 'pakalu papito');
 	t.is(user.handle, 'pakalupapito');
 	t.is(user.bio, 'started from gas station now we here.');
-	t.is(user.tweets, '2,246');
+	t.is(user.tweets, '2,252');
 	t.is(user.followers, '1.52M');
 	t.is(user.following, '177');
-	t.is(user.likes, '2,348');
+	t.is(user.likes, '2,347');
 });
